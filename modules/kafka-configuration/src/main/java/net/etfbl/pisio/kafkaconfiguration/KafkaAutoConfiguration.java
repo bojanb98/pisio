5e463b5f92d24e324c0416af2732bb9c67a56af1
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-06-14 15:33:29 +0200

    Fixed Kafka auto configuration

--- modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaAutoConfiguration.java
+++ modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaAutoConfiguration.java
@@ -2,11 +2,16 @@ package net.etfbl.pisio.kafkaconfiguration;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.TopicConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,5 +33,24 @@ public class KafkaAutoConfiguration {
                 .replicas(1)
                 .config(TopicConfig.COMPRESSION_TYPE_CONFIG, "zstd")
                 .build();
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://kafka-service:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
