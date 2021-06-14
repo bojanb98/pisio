0de72c270e28f1e6b4f0d79065ae9d89efe38b3d
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-06-14 16:50:31 +0200

    Fixed Kafka Auto configuration

--- modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaConsumerAutoConfiguration.java
+++ modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaConsumerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package net.etfbl.pisio.kafkaconfiguration;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -35,6 +36,8 @@ public class KafkaConsumerAutoConfiguration {
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://kafka-service:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
     }
 }
