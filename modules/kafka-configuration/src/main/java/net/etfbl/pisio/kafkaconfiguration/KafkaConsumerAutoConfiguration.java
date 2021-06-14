f630260300b804c42293011e4d5ab74f8bab4e7e
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-06-14 16:57:42 +0200

    Fixed KafkaConsumer Auto configuration

--- modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaConsumerAutoConfiguration.java
+++ modules/kafka-configuration/src/main/java/net/etfbl/pisio/kafkaconfiguration/KafkaConsumerAutoConfiguration.java
@@ -1,7 +1,7 @@
 package net.etfbl.pisio.kafkaconfiguration;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -36,8 +36,8 @@ public class KafkaConsumerAutoConfiguration {
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://kafka-service:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
 }
