0c6cf1025378501eeacff4ec1bab3c5bf59051be
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-06-25 09:51:16 +0200

    Added Kafka handler for GIF service

--- modules/gif-service/src/main/java/net/etfbl/pisio/gifservice/GifService.java
+++ modules/gif-service/src/main/java/net/etfbl/pisio/gifservice/GifService.java
@@ -2,6 +2,12 @@ package net.etfbl.pisio.gifservice;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GifService {
+
+    public byte[] createGif(List<byte[]> images) {
+        return null;
+    }
 }
