f3b1a3788b2c2c8501202247d22996ea6c0ff927
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-07-05 16:05:39 +0200

    Added WebSocket configuration and handler class

---
+++ modules/communication-service/src/main/java/net/etfbl/pisio/communicationservice/WebSocketConfiguration.java
@@ -0,0 +1,68 @@
+package net.etfbl.pisio.communicationservice;
+
+
+import lombok.AllArgsConstructor;
+import net.etfbl.pisio.authfilter.TokenValidationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebSocket
+@AllArgsConstructor
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    private static final String WEBSOCKET_TICKET_HEADER = "sec-websocket-protocol";
+
+    private final TokenValidationService tokenValidationService;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(new TextWebSocketHandler(), "/ws")
+                .addInterceptors(handshakeInterceptor())
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public HandshakeInterceptor handshakeInterceptor() {
+        return new HandshakeInterceptor() {
+
+
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws IOException {
+                List<String> tickets = serverHttpRequest.getHeaders().getOrEmpty(WEBSOCKET_TICKET_HEADER);
+                if (tickets.isEmpty()) {
+                    return false;
+                }
+
+                boolean success = tokenValidationService.isTokenValid(tickets.get(0));
+
+                if (!success) {
+                    return false;
+                }
+
+
+                map.put("username", "");
+                serverHttpResponse.getHeaders().add(WEBSOCKET_TICKET_HEADER, tickets.get(0));
+                return true;
+            }
+
+            @Override
+            public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+            }
+        };
+    }
+}
---
+++ modules/communication-service/src/main/java/net/etfbl/pisio/communicationservice/WebSocketHandler.java
@@ -0,0 +1,6 @@
+package net.etfbl.pisio.communicationservice;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+}
