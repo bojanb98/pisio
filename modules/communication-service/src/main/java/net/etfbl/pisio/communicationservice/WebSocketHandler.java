1d6877b17453aa4639315c628dd14f496dfc78bb
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-07-05 16:32:34 +0200

    Implemented session mappings

--- modules/communication-service/src/main/java/net/etfbl/pisio/communicationservice/WebSocketHandler.java
+++ modules/communication-service/src/main/java/net/etfbl/pisio/communicationservice/WebSocketHandler.java
@@ -1,6 +1,31 @@
 package net.etfbl.pisio.communicationservice;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        String username = getSessionUser(session);
+        sessions.put(username, session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+       String username = getSessionUser(session);
+       sessions.remove(username);
+    }
+
+    private String getSessionUser(WebSocketSession session) {
+        return session.getAttributes().get("username").toString();
+    }
 }
